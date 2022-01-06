6215d2de258f3affc0bfdbecbe2ce03bc8c19ec5
coelhovinicius <coelhovinicius@yahoo.com.br>
2022-01-06 19:16:39 -0300

    Dictionary - Exercicio Proposto

---
+++ Aula214_Dictionary_ExProposto/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Aula214_Dictionary_ExProposto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+                    while (!sr.EndOfStream)
+                    {
+                        string[] votingRecord = sr.ReadLine().Split(',');
+                        string candidate = votingRecord[0];
+                        int votes = int.Parse(votingRecord[1]);
+                        if (dictionary.ContainsKey(candidate))
+                        {
+                            dictionary[candidate] += votes;
+                        }
+                        else
+                        {
+                            dictionary[candidate] = votes;
+                        }
+                        /*string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                        string[] line = sr.ReadLine().Split(',');
+                        string name = line[0];
+                        int votes = int.Parse(line[1]);
+                        Console.WriteLine(name + "," + votes);*/
+                    }
+                    foreach (var item in dictionary)
+                    {
+                        Console.WriteLine(item.Key + ": " + item.Value);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
